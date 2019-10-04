2fdaaea8e492f8d9d852ef3c35970e5b2118950f
skdGT <skidru00@gmail.com>
2019-10-04 21:00:49 +0100

    deleted files

--- aeda1920_fp02_extra/aeda1920_fp02_extra/cmake-build-debug/lib/googletest-master/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/tiagopinto/OneDrive - Universidade do Porto/2º Ano/1º Semestre/AEDA/Prática/TP 2/aeda1920_fp02_extra/lib/googletest-master/googlemock
-# Build directory: /Users/tiagopinto/OneDrive - Universidade do Porto/2º Ano/1º Semestre/AEDA/Prática/TP 2/aeda1920_fp02_extra/cmake-build-debug/lib/googletest-master/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
