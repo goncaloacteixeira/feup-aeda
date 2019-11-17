fd654e4767550291112c18a36369eaeea37209f4
skdGT <skidru00@gmail.com>
2019-11-17 01:55:51 +0000

    minor changes

--- tests/aeda1718_ci2_googleTest/cmake-build-debug/lib/googletest-master/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /cygdrive/c/Users/skidr/OneDrive/Desktop/git/AEDA/tests/aeda1718_ci2_googleTest/lib/googletest-master
-# Build directory: /cygdrive/c/Users/skidr/OneDrive/Desktop/git/AEDA/tests/aeda1718_ci2_googleTest/cmake-build-debug/lib/googletest-master
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
