f25533406cab9699dac49f10bae52e6f9e0dec31
skdGT <skidru00@gmail.com>
2019-11-18 00:04:58 +0000

    tests e) and f) passed

--- tests/aeda1617_ci2_googleTest/Tests/AfterSalesService.cpp
+++ tests/aeda1617_ci2_googleTest/Tests/AfterSalesService.cpp
@@ -59,19 +59,44 @@ vector<Article*> AfterSalesService::pickPresentsFromTable(long client) {
  *       the one with lower present number should have priority (clients should not wait too much);
  * (iii) else, maintain article order in the table.
  */
+
+bool foo(Article* a1 , Article* a2) {
+    if (a1->getPresentNumber() + 2 < a2->getPresentNumber())
+        return true;
+    return a1->getArrivalNumber() < a2->getArrivalNumber();
+}
+
 void AfterSalesService::sortArticles() {
+    vector<Article* > aux = {presentsTable.begin(), presentsTable.begin() + 10};
+    vector<Article* > homePresents;
 
-	// TODO
+    for (auto it = aux.begin(); it != aux.end(); it++)
+        if ((*it)->getDeliverHome()) {
+            homePresents.push_back(*it);
+            it = --aux.erase(it);
+        }
 
+    sort(aux.begin(), aux.end(), foo);
+
+    aux.insert(aux.end(), homePresents.begin(), homePresents.end());
+
+    int i = 0;
+    while (i < 10) {
+        presentsTable[i] = aux[i];
+        i++;
+    }
 }
 
 /**
  * Move the first presents in the table to the queue of presents to wrap, taking into account the size of the queue.
  */
 void AfterSalesService::enqueueArticles() {
-
-	// TODO
-
+    int leftPlaces = toWrapQueueSize - toWrap.size();
+    while (leftPlaces != 0 && !presentsTable.empty()) {
+        toWrap.push(presentsTable[0]);
+        leftPlaces--;
+        presentsTable.erase(presentsTable.begin());
+    }
 }
 
 /**
