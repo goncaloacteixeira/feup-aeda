78de97eed75963b8ba82f1b9a66ae3453fd2c173
skdGT <Teixeira_2000>
2019-11-27 22:31:47 +0000

    all tests passed successfully

--- aeda1920_fp09/Tests/jogador.cpp
+++ aeda1920_fp09/Tests/jogador.cpp
@@ -14,9 +14,18 @@ unsigned Jogador::apostasNoNumero(unsigned num) const
 	return count;
 }
 
-//a alterar
 tabHAposta Jogador::apostasPremiadas(const tabHInt & sorteio) const
 {
 	tabHAposta money;
+
+	for (auto &aposta : apostas) {
+	    int aux = 0;
+	    for (auto &key : sorteio)
+	        if (aposta.contem(key)) {
+	            aux++;
+	        }
+	    if (aux > 3)
+	        money.insert(aposta);
+	}
 	return money;
 }
