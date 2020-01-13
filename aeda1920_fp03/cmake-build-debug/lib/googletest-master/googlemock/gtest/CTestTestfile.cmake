94e86394657d32a1eb1f875b5495ea12db1eb045
skdGT <skidru00@gmail.com>
2020-01-13 18:39:27 +0000

    removed some files added pdfs

--- aeda1920_fp03/cmake-build-debug/lib/googletest-master/googlemock/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /cygdrive/c/Users/skidr/OneDrive/Desktop/new/lib/googletest-master/googletest
-# Build directory: /cygdrive/c/Users/skidr/OneDrive/Desktop/new/cmake-build-debug/lib/googletest-master/googlemock/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- tests/aeda1819_ci1_googleTest/cmake-build-debug/lib/googletest-master/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /cygdrive/c/Users/skidr/OneDrive/Desktop/git/AEDA/tests/aeda1819_ci1_googleTest/lib/googletest-master/googlemock
-# Build directory: /cygdrive/c/Users/skidr/OneDrive/Desktop/git/AEDA/tests/aeda1819_ci1_googleTest/cmake-build-debug/lib/googletest-master/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
